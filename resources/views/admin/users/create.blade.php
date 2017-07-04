5fa72766479bcd10a7b7e8d18d8e0aea24a342ce
vagrant <vagrant@homestead>
2017-07-04 08:45:26 +0000

    create with photo

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -25,8 +25,8 @@
 
 	</div>
 	<div class="form-group">
-		{!! Form::label('file','Image:') !!}
-		{!! Form::file('file',['class'=>'form-control']) !!}
+		{!! Form::label('photo_id','Photo:') !!}
+		{!! Form::file('photo_id',['class'=>'form-control']) !!}
 
 	</div>
 	<div class="form-group">
